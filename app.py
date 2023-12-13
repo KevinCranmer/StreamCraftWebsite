5c3b33c0e702acad203a6b51ad771dbc6d2c8e53
KevinCranmer <ktcranme@asu.edu>
2023-12-13 01:46:56 -0600

    initial

---
+++ app.py
@@ -0,0 +1,52 @@
+import json
+import os
+from flask import Flask, request, render_template, redirect, url_for
+import requests
+import urllib
+app = Flask(__name__)
+
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/codes", methods=['GET'])
+def codes():
+    username = request.args['state']
+    code = request.args['code']
+    url = "https://id.twitch.tv/oauth2/token"
+    headers = {"Content-Type" : "application/x-www-form-urlencoded"}
+    data = {
+        "client_id" : client_id,
+        "client_secret" : client_secret,
+        "code" : code,
+        "grant_type" : "authorization_code",
+        "redirect_uri" : "http://localhost:5000/results"
+    }
+    r = requests.post(url, data=data, headers=headers)
+    access_token = json.loads(r.text)['access_token']
+    refresh_token = json.loads(r.text)['refresh_token']
+
+    url = "https://api.twitch.tv/helix/users?login=" + username
+    headers = {
+        "Authorization" : "Bearer " + access_token,
+        "Client-Id" : client_id
+        }
+    r = requests.get(url, headers=headers)
+    broadcaster_id = json.loads(r.text)['data'][0]['id']
+    return render_template('codes.html', access_token=access_token, refresh_token=refresh_token, broadcaster_id=broadcaster_id)
+
+@app.route("/authorize", methods=['POST'])
+def authorize():
+    username = request.form['username']
+    url = "https://id.twitch.tv/oauth2/authorize?"
+    params = {
+        "response_type" : "code",
+        "client_id" : client_id,
+        "redirect_uri" : "http://localhost:5000/codes",
+        "scope" : "moderator:read:followers",
+        "state" : username
+        }
+    return redirect(url + urllib.parse.urlencode(params))
